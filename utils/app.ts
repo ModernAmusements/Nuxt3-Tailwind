049e85408b9aa52d607490e0f135a657268afa5d
shady <shady@epunks.de>
2022-06-18 14:54:15 +0200

    first commit

---
+++ utils/app.ts
@@ -0,0 +1,34 @@
+import { ThemeManager } from './theme'
+import { LanguageManager } from './lang'
+
+export interface IApp {
+  name: string
+  author: {
+    name: string
+    link: string
+  }
+}
+
+export function AppSetup() {
+  // declare app information
+  const app: IApp = {
+    name: 'Modern Amusment Nuxt 3 Starter',
+    author: {
+      name: 'Shady Tawfik',
+      link: 'https://github.com/viandwi24',
+    },
+  }
+  useState('app', () => app)
+
+  // use theme manager
+  const themeManager = ThemeManager()
+
+  // use language manager
+  const languageManager = LanguageManager()
+
+  return {
+    app,
+    themeManager,
+    languageManager,
+  }
+}
